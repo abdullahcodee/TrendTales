0a01cd69e41b46de6944cc2ad724e8cb130d7f8a
Abduallah El-Khodary <63024842+abdullahcodee@users.noreply.github.com>
2024-08-28 18:57:01 +0300

    The Books APP has been removed

--- book/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .import views
-
-urlpatterns = [
-    path("",views.StartingPageView.as_view(), name="starting_page" ),
-    path("books",views.Books, name="books" ),
-    path("books/<slug:slug>/", views.bookDetailsView,name="book-details"),
-    path("books/category/", views.books_category,name="books-category"),
-]
--- my_site/settings.py
+++ my_site/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "blog",
-    "book",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
